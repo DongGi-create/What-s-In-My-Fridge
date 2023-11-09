e9c6c617dc960006248c25427692d43200111583
KoharuDaisukii <bong1926@naver.com>
2023-11-09 15:55:45 +0900

    Relation->Class

---
+++ src/Phase3/Require.java
@@ -0,0 +1,67 @@
+package Phase3;
+
+import java.util.Objects;
+
+public class Require {
+	private int recipe_ID;
+	private int ingredient_ID;
+	private int quantity;
+	private String unit;
+
+	public Require(int recipe_ID, int ingredient_ID, int quantity, String unit) {
+		this.recipe_ID = recipe_ID;
+		this.ingredient_ID = ingredient_ID;
+		this.quantity = quantity;
+		this.unit = unit;
+	}
+
+	public int getRecipe_ID() {
+		return recipe_ID;
+	}
+
+	public void setRecipe_ID(int recipe_ID) {
+		this.recipe_ID = recipe_ID;
+	}
+
+	public int getIngredient_ID() {
+		return ingredient_ID;
+	}
+
+	public void setIngredient_ID(int ingredient_ID) {
+		this.ingredient_ID = ingredient_ID;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String toString() {
+		return "Require [recipe_ID=" + recipe_ID + ", ingredient_ID=" + ingredient_ID + ", quantity=" + quantity
+				+ ", unit=" + unit + "]";
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Require other = (Require) obj;
+		return ingredient_ID == other.ingredient_ID && quantity == other.quantity && recipe_ID == other.recipe_ID
+				&& Objects.equals(unit, other.unit);
+	}
+
+}
---
+++ src/Phase3/Users.java
@@ -0,0 +1,88 @@
+package Phase3;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Users {
+	private String user_ID;
+	private String password;
+	private String name;
+	private String email;
+	private Date birth;
+	private char sex;
+
+	public Users(String user_ID, String user_PW, String name, String email, Date birth, char sex) {
+		this.user_ID = user_ID;
+		this.password = user_PW;
+		this.name = name;
+		this.email = email;
+		this.birth = birth;
+		this.sex = sex;
+	}
+
+	public String getUser_ID() {
+		return user_ID;
+	}
+
+	public void setUser_ID(String user_ID) {
+		this.user_ID = user_ID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+	public String toString() {
+		return "Users [user_ID=" + user_ID + ", password=" + password + ", name=" + name + ", email=" + email
+				+ ", birth=" + birth + ", sex=" + sex + "]";
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Users other = (Users) obj;
+		return Objects.equals(birth, other.birth) && Objects.equals(email, other.email)
+				&& Objects.equals(name, other.name) && Objects.equals(password, other.password) && sex == other.sex
+				&& Objects.equals(user_ID, other.user_ID);
+	}
+}
