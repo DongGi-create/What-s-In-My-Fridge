df792b4135799d1be78a88462b5df862ab7eb770
KoharuDaisukii <bong1926@naver.com>
2023-11-10 15:42:36 +0900

    .

--- Phase3/src/Phase3JDBC.java
+++ Phase3/src/Phase3JDBC.java
@@ -9,33 +9,11 @@ public class Phase3JDBC {
 
 	public static void main(String[] args) {
 		Connection conn = null;
+		Users user = null;
 		JDBCDriver.load();
 		conn = JDBCDriver.getConnection(URL, DB_ID, DB_PW);
-		Users user = initialPage(conn);
-
+		user = LoginWIF.initialPage(conn);
+		System.out.println(user);
 		JDBCDriver.close(conn);
-	}
-
-	public static Users initialPage(Connection conn) {
-		int select;
-		Users user = null;
-		Scanner keyboard = new Scanner(System.in);
-
-		do {
-			System.out.println("1. 로그인 2. 회원가입, 3.종료");
-			select = keyboard.nextInt();
-			if (select == 1) {
-				user = LoginWIF.loginAccount(conn);
-				if (user != null)
-				{	
-					System.out.println(user.getUser_ID() + "님 환영합니다.");
-					break;
-				}
-				else
-					System.out.println("해당하는 유저가 존재하지 않습니다.");
-			}
-		} while (select != 3);
-
-		return user;
 	}
 }
