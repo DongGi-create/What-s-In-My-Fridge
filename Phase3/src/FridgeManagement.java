05df8ac5e0bdc442be88d49bdcdafefb47fe3d92
KoharuDaisukii <bong1926@naver.com>
2023-11-19 00:09:36 +0900

    implemented removing Own-record

--- Phase3/src/FridgeManagement.java
+++ Phase3/src/FridgeManagement.java
@@ -21,7 +21,7 @@ public class FridgeManagement
 				// 냉장고 속 재료를 먼저 보여줌
 				showIngredients(conn, user);
 
-				System.out.println("1. 재료 추가하기 2. 재료 빼기 3, 뒤로");
+				System.out.println("1. 재료 넣기 2. 재료 빼기 3. 뒤로");
 				select = keyboard.nextInt();
 				if (select == 1)
 				{
@@ -29,7 +29,7 @@ public class FridgeManagement
 				}
 				else if (select == 2)
 				{
-
+					removeIngredient(conn, user);
 				}
 				else
 				{
@@ -60,19 +60,20 @@ public class FridgeManagement
 			PreparedStatement pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, user.getUser_ID());
 			ResultSet rs = pstmt.executeQuery();
+			int count = 0;
 			while (rs.next())
 			{
-				System.out.println(rs.getString(1) + ": " + rs.getInt(2) + "(" + rs.getString(3) + ")");
+				System.out
+						.println(++count + ". " + rs.getString(1) + ": " + rs.getInt(2) + "(" + rs.getString(3) + ")");
 			}
 			System.out.println();
-			rs.close();
 			pstmt.close();
+			rs.close();
 		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void addIngredient(Connection conn, Users user)
@@ -85,12 +86,12 @@ public class FridgeManagement
 		Ingredient insertIngredient = null;
 		Own insertOwn = null;
 
-		System.out.println("[냉장고 관리 - 재료 추가하기]");
+		System.out.println("[냉장고 관리 - 재료 넣기]");
+		keyboard.nextLine();
 		// 재료 이름 입력
 		do
 		{
-			System.out.print("재료의 이름을 입력해주세요: ");
-			keyboard.nextLine();
+			System.out.print("넣을 재료의 이름을 입력해주세요: ");
 			String input = keyboard.nextLine();
 
 			if (input.length() > 40)
@@ -152,7 +153,7 @@ public class FridgeManagement
 		// 입력한 재료를 이미 가지고 있다면 알려줌.
 		if (alreadyHaving)
 		{
-			System.out.println("이미 \'" + insertIngredient.getIngredient_Name() + "\'를 \'" + insertOwn.getQuantity()
+			System.out.println("이미 \'" + insertIngredient.getIngredient_Name() + "\'을(를) \'" + insertOwn.getQuantity()
 					+ "(" + insertOwn.getUnit() + ")' 만큼 가지고 계시네요.");
 			query = "UPDATE OWN SET OWN.QUANTITY = ?, OWN.UNIT = ? WHERE OWN.USER_ID = ? AND OWN.INGREDIENT_ID = ?";
 		}
@@ -165,7 +166,7 @@ public class FridgeManagement
 		{
 			try
 			{
-				System.out.println("정말로 재료를 추가하실 건가요?");
+				System.out.println("정말로 " + insertIngredient.getIngredient_Name() + "을(를) 추가하실 건가요?");
 				System.out.println("1. 예 2. 아니요");
 				select = keyboard.nextInt();
 				if (select == 2)
@@ -214,6 +215,74 @@ public class FridgeManagement
 		{
 			System.out.println("잘못된 입력을 하셨습니다.");
 		}
+	}
 
+	public static void removeIngredient(Connection conn, Users user)
+	{
+		String query = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int select = 1;
+
+		System.out.println("[냉장고 관리 - 재료 빼기]");
+		System.out.println(user.getUser_ID() + "님의 냉장고에 있는 재료들이에요.");
+		System.out.println("-------------------");
+		do
+		{
+			int count = 0;
+			try
+			{
+				query = "SELECT\r\n" + "	I.INGREDIENT_NAME,\r\n" + "	O.QUANTITY,\r\n" + "	O.UNIT\r\n" + "FROM\r\n"
+						+ "	USERS      U,\r\n" + "	OWN        O,\r\n" + "	INGREDIENT I\r\n" + "WHERE\r\n"
+						+ "	    U.USER_ID = O.USER_ID\r\n" + "	AND I.INGREDIENT_ID = O.INGREDIENT_ID\r\n"
+						+ "	AND U.User_ID = ?\r\n" + "ORDER BY\r\n" + "	I.INGREDIENT_NAME";
+				// 빠꾸 가능한 ResultSet을 위해
+				pstmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				pstmt.setString(1, user.getUser_ID());
+				rs = pstmt.executeQuery();
+
+				while (rs.next())
+				{
+					System.out.println(
+							++count + ". " + rs.getString(1) + ": " + rs.getInt(2) + "(" + rs.getString(3) + ")");
+				}
+				System.out.println();
+
+				System.out.println("뺄 재료의 번호를 입력해주세요. 취소하려면 0을 입력해주세요.");
+				select = keyboard.nextInt();
+				if (select > count || select < 0)
+					throw new InputMismatchException();
+
+				rs.beforeFirst();
+				for (int i = 0; i < select; i++)
+				{
+					rs.next();
+				}
+
+				if (!rs.isBeforeFirst() && count > 0)
+				{
+					query = "DELETE FROM OWN WHERE USER_ID = ? AND INGREDIENT_ID = (SELECT INGREDIENT_ID FROM INGREDIENT WHERE INGREDIENT_NAME = ?)";
+					PreparedStatement pstmt2 = conn.prepareStatement(query);
+					pstmt2.setString(1, user.getUser_ID());
+					pstmt2.setString(2, rs.getString(1));
+					int result = pstmt2.executeUpdate();
+					if (result > 0)
+						System.out.println("성공적으로 " + rs.getString(1) + "의 제거가 완료되었습니다.");
+					else
+						System.out.println("왜인지는 모르겠는데 실패");
+					pstmt2.close();
+				}
+				pstmt.close();
+				rs.close();
+			}
+			catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			catch (InputMismatchException e)
+			{
+				System.out.println("0~" + count + " 사이의 숫자를 입력해주세요.");
+			}
+		} while (select != 0);
 	}
 }
